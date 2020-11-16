1c7ab8619938a40afafa6ede913ddc292ea08cb8
unknown <jasmineakbari@yahoo.com>
2020-11-15 21:27:07 -0500

    added icons and started api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var getWeather = function(city) {
     //format the github api url
-    var apiUrl = fetch("a2dd844fa915b1c2936067736b330124")
+    var apiUrl = fetch("api.openweathermap.org/data/2.5/forecast?zip=94040,us&appid=a2dd844fa915b1c2936067736b330124")
     .then(console.log(apiUrl))
 }
 
