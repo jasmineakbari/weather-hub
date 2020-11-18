a5697d53b7ee8ef947f10c0f49491263a717daa0
unknown <jasmineakbari@yahoo.com>
2020-11-17 20:26:17 -0500

    added error script for fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
-var getWeather = function() {
+var nameInputEl = document.querySelector("#city-name")
+var searchHistory = document.querySelector(".recent-searches")
+var futureWeather = document.querySelector(".future")
+var currentWeather = document.querySelector(".today")
+
+var getWeather = function(city) {
     //format the github api url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + "orlando" + "&appid=" + "03b57575688ded7ffa5d8cffaa391e7a"
-    console.log(apiUrl)
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + "Orlando" + "&appid=" + "03b57575688ded7ffa5d8cffaa391e7a"
+    
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Internet");
+    })
 }
 
 getWeather()
