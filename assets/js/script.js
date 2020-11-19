9700cc434980c800a2ff55ac87a984ceec923f69
unknown <jasmineakbari@yahoo.com>
2020-11-18 19:17:37 -0500

    able to search by city name to pull object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,64 @@
-var nameInputEl = document.querySelector("#city-name")
+var cityInputEl = document.querySelector("#city-name")
+var cityFormEl = document.querySelector(".form-inline")
 var searchHistory = document.querySelector(".recent-searches")
 var futureWeather = document.querySelector(".future")
 var currentWeather = document.querySelector(".today")
 
+// fetch api data function
 var getWeather = function(city) {
-    //format the github api url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + "Orlando" + "&appid=" + "03b57575688ded7ffa5d8cffaa391e7a"
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + "03b57575688ded7ffa5d8cffaa391e7a"
     
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+    .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data)
+                console.log(data, city)
             })
         } else {
             alert("Error: " + response.statusText);
         }
     })
     .catch(function(error) {
-        alert("Unable to connect to Internet");
+        alert("Network Connection Error");
     })
 }
 
-getWeather()
+// form handler for retrieving city object
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event)
+
+    var cityName = cityInputEl.value.trim()
+
+    if (cityName) {
+        getWeather(cityName)
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter Valid U.S City Name")
+    }
+}
+
+// display city information function
+var displayCityWeather = function(weather, city) {
+    // will need to add city name to recent searches as button
+
+    // clear old content first
+    futureWeather.textContent = ""
+    currentWeather.textContent = ""
+
+    for (var i = 0; i < currentWeather.length; i++) {
+        // format City Name
+        var cityName = city
+
+        // create card and apply attributes
+        var cityCardEl = document.createElement("div")
+        cityCardEl.classList = "col-4 card text-white bg-primary mb-3"
+        cityCardEl.textContent = cityName
+
+        currentWeather.appendChild(cityCardEl)
+    }
+
+}
+
+// Event Listeners
+cityFormEl.addEventListener("submit", formSubmitHandler)
